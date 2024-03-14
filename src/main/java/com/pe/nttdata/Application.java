6d4e807bf4d51749cce15651fcb476ece2d9517f
Hugo Oliveros <hugo.oliveros@gmail.com>
2024-03-14 14:57:10 -0500

    Semana 2 - Cambios 08/03/2024

--- src/main/java/com/pe/nttdata/Application.java
+++ src/main/java/com/pe/nttdata/Application.java
@@ -4,6 +4,7 @@ import com.pe.nttdata.config.AuditorAwareImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +34,7 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @Configuration
 @EnableReactiveMongoRepositories
 @EnableReactiveMongoAuditing
+@EnableDiscoveryClient
 public class Application implements CommandLineRunner {
 
   public static void main(String[] args) {
